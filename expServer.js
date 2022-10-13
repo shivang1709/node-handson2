aa44de7fc741b9ee77b0dc7a1f4f62c50d118e7e
shivang singh <busy.shivangsingh@gmail.com>
2022-10-14 02:03:06 +0530

    handson 2

---
+++ expServer.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 9001;
+const data = {
+    Name: 'Shivang',
+    LastName: 'Singh',
+    Age: 26,
+    city: 'Hardoi'
+}
+const jasonData = JSON.stringify(data);
+app.get('/', (req, res) => {
+    res.send(`<h1>Assignment 2</h1>
+    <p>Go to /api route to see html data </p>
+    <p>Go to /main route to see json data </p>`)
+})
+app.get('/api', (req, res) => {
+    res.write(`<h1>#What is express js? </h1>
+    <h2>1.Express. js is a free and open-source web application framework for Node. js.</h2> 
+    <h2>2.It is used for designing and building web applications quickly and easily.</h2>`)
+    res.end();
+})
+app.get('/main', (req, res) => {
+    res.write(jasonData)
+    res.end();
+})
+
+app.listen(port, () => {
+    console.log(`Connected to port ${port} ✅`);
+})
